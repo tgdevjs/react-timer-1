64c7d41680cc90a4fe47919d9596c640bc914d18
tg <tgdevjs@gmail.com>
2016-08-28 21:27:51 -0400

    Add countdown functionality.

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -5,11 +5,46 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
   getInitialState: function () {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
   },
-  onHandleSetCountdown: function (seconds) {
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus != prevState.countdownStatus) {
+      switch(this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.stopTimer();
+          this.setState({count: 0});
+          break;
+        case 'paused':
+          this.stopTimer();
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >=0 ? newCount : 0
+      });
+
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
+    }, 1000);
+  },
+  stopTimer: function () {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  handleSetCountdown: function (seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus:'started'
     });
   },
   render: function () {
@@ -18,7 +53,7 @@ var Countdown = React.createClass({
       <div>
         <h3>Countdown Component</h3>
         <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.onHandleSetCountdown}/>
+        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
       </div>
     );
   }
